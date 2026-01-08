9cd272d1df1228e50d02204210b14755406415f8
prajitashrestha-star <prajitashrestha845@gmail.com>
2026-01-08 16:33:42 +0545

    Added routes

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -4,7 +4,7 @@ const express= require("express").Router();
 const { addUser, getActiveUsers, getAllUsers, getUsersById, updateUser, deleteUser, loginUser } = require('../controllers/userController')
 
 express.post("/register", addUser);
-express.post("/register", getActiveUsers);
+// express.post("/register", getActiveUsers);
 express.get("/getallUsers", getAllUsers);
 express.get("/getUserByid/:uid", getUsersById);
 express.put("/updateUserByid/:id", updateUser);
