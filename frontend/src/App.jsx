410691ab25ea84b1dbd529afdb6db04c680588af
salina12305 <prakritistha06@gmail.com>
2026-01-22 21:05:20 +0545

    Added AdminBooking where admin can see who has booked packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,9 @@ import { Toaster } from 'react-hot-toast';
 import ApproveAgents from './pages/ApproveAgents';
 import AdminUsers from './pages/AdminUsers';
 import AdminDashboard from './pages/AdminDashboard';
+import TravelAgentDashboard from './pages/TravelAgentDashboard';
 import TravelAgentPackages from './pages/TravelAgentPackages';
+import AdminBooking from './pages/AdminBooking';
 function App(){
   return (
     <Router>
@@ -27,10 +29,12 @@ function App(){
         <Route path= "/uploadimage" element = {<ProfileUpload/>}/>
         <Route path="/approveagents" element={<ApproveAgents />} />
         <Route path="/adminusers" element={<AdminUsers/>}/>
+        <Route path="/adminbookings" element={<AdminBooking />} />
         <Route path="/admindashboard" element={
           <ProtectedRoute allowedRoles={['admin']} element={<AdminDashboard/>}/>}/>
       <Route path="/forgotpassword" element={<ForgotPassword/>}/>
       <Route path="/resetpassword" element={<ResetPassword/>}/>
+      <Route path="/travelagentdashboard" element={<TravelAgentDashboard/>}/> 
       <Route path="/agentpackages" element={<TravelAgentPackages/>}/>
       </Routes>
     </Router>
